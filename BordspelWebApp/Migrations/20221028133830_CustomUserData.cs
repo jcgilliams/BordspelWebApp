7de6bf9cb17e6ae48ae3475f4e3a0a0c6f3eeba3
jcgilliams <jc.gilliams@gmail.com>
2022-11-16 14:07:41 +0100

    klaarOmTeLayouten

---
+++ BordspelWebApp/Migrations/20221028133830_CustomUserData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BordspelWebApp.Migrations
+{
+    public partial class CustomUserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Naam",
+                schema: "BordspelWebApp",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Naam",
+                schema: "BordspelWebApp",
+                table: "AspNetUsers");
+        }
+    }
+}
