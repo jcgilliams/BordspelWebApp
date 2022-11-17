b5724972abb76fb7f23576fb530a6dfd93175536
jcgilliams <jc.gilliams@gmail.com>
2022-11-17 16:55:28 +0100

    layout-navigatie

---
+++ BordspelWebApp/Controllers/CollectieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BordspelWebApp.Controllers
+{
+    public class CollectieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
