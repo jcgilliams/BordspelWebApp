1b55e66524f2dec86500c1c00454ecb8adafae99
jcgilliams <jc.gilliams@gmail.com>
2022-12-03 10:33:07 +0100

    bordspellen tonen + details

--- BordspelWebApp/Controllers/HomeController.cs
+++ BordspelWebApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using BordspelWebApp.Models;
+using BordspelWebApp.ViewModels.Lists;
+using BordspelWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,21 +8,33 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using BordspelWebApp.Data;
+using BordspelWebApp.ViewModels.Details;
+using Microsoft.EntityFrameworkCore;
 
 namespace BordspelWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+        public readonly BordspelWebAppContext _context;
+        public HomeController(BordspelWebAppContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            ListBordspelViewModel vm = new ListBordspelViewModel()
+            {
+                Bordspellen = _context.Bordspellen.OrderBy(x=>x.Naam).ToList()
+            };
+            return View(vm);
         }
 
         public IActionResult Privacy()
@@ -36,6 +50,63 @@ namespace BordspelWebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult DetailsBordspel(int id)
+        {
+            Bordspel bordspel = _context.Bordspellen.Where(b => b.Id == id)
+                .FirstOrDefault();
+
+            ICollection<BordspelPersoon> BordspelPersonen = _context.BordspelPersonen.Where(b => b.BordspelId == id)
+                .Include(p=>p.Persoon)
+                .Include(r=>r.Rol)
+                .ToList();
+
+            ICollection<Uitgave> Uitgaven = _context.Uitgaven.Where(b => b.BordspelId == id)
+                .Include(u => u.Uitgeverij)
+                .ToList();
+
+            if (bordspel != null)
+            {
+                DetailsBordspelViewModel vm = new DetailsBordspelViewModel()
+                {
+                    Id = bordspel.Id,
+                    Naam = bordspel.Naam,
+                    Jaar = bordspel.Jaar,
+                    Beschrijving = bordspel.Beschrijving,
+                    MinAantalSpelers = bordspel.MinAantalSpelers,
+                    MaxAantalSpelers = bordspel.MaxAantalSpelers,
+                    MinSpeeltijd = bordspel.MinSpeeltijd,
+                    MaxSpeeltijd = bordspel.MaxSpeeltijd,
+                    Leeftijd = bordspel.Leeftijd,
+                    Afbeelding = bordspel.Afbeelding,
+                    BordspelPersonen = BordspelPersonen,
+                    Uitgaven = Uitgaven
+                };
+
+                return View(vm);
+            }
+            else
+            {
+                ListBordspelViewModel vm = new ListBordspelViewModel()
+                {
+                    Bordspellen = _context.Bordspellen.OrderBy(x => x.Naam).ToList()
+                };
+                return View("Index", vm);
+            }
+        }
+        public IActionResult Search(ListBordspelViewModel viewModel)
+        {
+            if (!string.IsNullOrEmpty(viewModel.SpelZoeken))
+            {
+                viewModel.Bordspellen = _context.Bordspellen.Where(p => p.Naam.Contains(viewModel.SpelZoeken)).ToList();
+            }
+            else
+            {
+                viewModel.Bordspellen = _context.Bordspellen.ToList();
+            }
+
+            return View("Index", viewModel);
         }
     }
 }
---
+++ BordspelWebApp/ViewModels/Lists/ListBordspelViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BordspelWebApp.Models;
+using System.Collections.Generic;
+
+namespace BordspelWebApp.ViewModels.Lists
+{
+    public class ListBordspelViewModel
+    {
+        public ICollection<Bordspel> Bordspellen { get; set; }
+        public string SpelZoeken { get; set; }
+    }
+}
